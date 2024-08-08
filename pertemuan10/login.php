dc779dc1ae37e2793982611224d031bd0a15c754
Alwiii <210209501011@student.unm.ac.id>
2024-08-08 15:29:48 +0800

    menambahkan fitur login

---
+++ pertemuan10/login.php
@@ -0,0 +1,57 @@
+<?php
+
+require 'function.php';
+
+if (isset($_POST['login'])) {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // cek username 
+    $result = mysqli_query($db, "SELECT * FROM user WHERE username = '$username'");
+
+    if (mysqli_num_rows($result) === 1) {
+        // cek password
+        $row = mysqli_fetch_assoc($result);
+        if (password_verify($password, $row["password"])) {
+            header("Location: index2.php");
+            exit;
+        }
+    }
+    $error = true;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <h1>Halaman Login</h1>
+
+    <?php if (isset($error)) : ?>
+        <p style="color: red; font-style:italic">Username / password salah</p>
+    <?php endif; ?>
+    <form action="" method="post">
+        <ul>
+            <li>
+                <label for="username">Username :</label>
+                <input type="text" name="username" id="username">
+            </li>
+            <li>
+                <label for="password">Password :</label>
+                <input type="password" name="password" id="password">
+            </li>
+            <li>
+                <button type="submit" name="login">Login</button>
+            </li>
+        </ul>
+    </form>
+</body>
+
+</html>
